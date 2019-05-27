89fa37e018ff213dc97346efcd6015f58f1de830
Hanna Jeong <id0242han@naver.com>
2019-05-27 09:53:08 +0900

    초기 커밋.

---
+++ TeaBagMaker/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeaBagMaker
+{
+    public partial class Form1 : Form
+    {
+
+        int CountOrgNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void TeaList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
